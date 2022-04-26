f9d9c89d1ba4892d76952998870934fc6237ee63
bgw <giung609@naver.com>
2022-04-26 15:41:27 +0900

    test1

---
+++ TEST1/Test_Util_1/Test_Util_1_Source.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Util_1
+{
+    public class Test_Util_1_Source
+    {
+        public string Instance_Name = "Test_Util_1_Source";
+    }
+}
