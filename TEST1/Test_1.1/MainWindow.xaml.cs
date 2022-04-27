e25a479c7b0ec862d940ca4fae43e423596819f0
bgw <giung609@naver.com>
2022-04-27 09:44:41 +0900

    sub module 2.1

--- TEST1/Test_1.1/MainWindow.xaml.cs
+++ TEST1/Test_1.1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using Test_Module_1._1;
+using Test_Module_2._1;
 
 namespace Test_1._1
 {
@@ -9,15 +10,17 @@ namespace Test_1._1
     public partial class MainWindow : Window
     {
         Module_1_1 Module_1_1;
+        Module_2_1 Module_2_1;
 
         public MainWindow()
         {
             InitializeComponent();
 
             Module_1_1 = new Module_1_1();
+            Module_2_1 = new Module_2_1();
 
             View_Text.Text = $"{Module_1_1.Module_Name} ({Module_1_1.Module_Function(0, 1)})";
-            //View_ShareText.Text = Test_Share_2_Source.Sharing_Name;
+            View_ShareText.Text = $"{Module_2_1.Module_Name} ({Module_2_1.Module_Function(1, 1)})";
         }
     }
 }
